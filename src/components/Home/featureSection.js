876a65b9b191ba4a2e23282d2613040d6afeb827
Rahul Ramesh <cerebralblade@gmail.com>
2024-07-27 09:44:25 +0100

    new changes\

--- src/components/Home/featureSection.js
+++ src/components/Home/featureSection.js
@@ -7,36 +7,35 @@ const FeatureSection = () => {
       <Fade bottom cascade>
         <div className="w-full flex flex-row justify-between mt-10 xxs:flex-col xs:flex-col sm:flex-row">
           <div className="rounded-xl w-full h-52 flex flex-col content-center justify-center align-middle text-center m-5 bg-gradient-to-r from-lightblack to-lightblack hover:from-purple hover:to-pink transition duration-500 ease-in-out">
-            <h1 className="text-white text-5xl">2.4M</h1>
-            <h3 className="text-white">Days turn around</h3>
+            <h1 className="text-white text-2xl">Web Application Pentest</h1>
+
           </div>
           <div className="rounded-xl w-full h-52 flex flex-col content-center justify-center align-middle text-center m-5 bg-gradient-to-r from-lightblack to-lightblack hover:from-purple hover:to-pink transition duration-500 ease-in-out">
-            <h1 className="text-white text-5xl">7M</h1>
-            <h3 className="text-white">Days turn around</h3>
+            <h1 className="text-white text-2xl">Managed Vulnerability Scanning</h1>
+          
           </div>
           <div className="rounded-xl w-full h-52 flex flex-col content-center justify-center align-middle text-center m-5 bg-gradient-to-r from-lightblack to-lightblack hover:from-purple hover:to-pink transition duration-500 ease-in-out">
-            <h1 className="text-white text-5xl">7.4%</h1>
-            <h3 className="text-white">Days turn around</h3>
+            <h1 className="text-white text-2xl">Network Pentest</h1>
+
           </div>
           <div className="rounded-xl w-full h-52 flex flex-col content-center justify-center align-middle text-center m-5 bg-gradient-to-r from-lightblack to-lightblack hover:from-purple hover:to-pink transition duration-500 ease-in-out">
-            <h1 className="text-white text-5xl">49K</h1>
-            <h3 className="text-white">Days turn around</h3>
+            <h1 className="text-white text-2xl"> Wireless Pentest</h1>
+
           </div>
         </div>
       </Fade>
       <Fade bottom cascade>
         <div className="mt-10 px-8">
           <h1 className="text-white text-7xl font-bold xxs:text-2xl xs:text-2xl sm:text-3xl lg:text-5xl xl:text-7xl">
-            From rough design files, to <br></br>
-            powerful products
+            Why Hawkeye Security?
           </h1>
           <h2 className="text-white text-sm mt-5 opacity-50">
-            Lorem Ipsum is simply dummy text of the printing and typesetting
-            industry. Lorem Ipsum has been the industry's standard dummy text
-            ever
-            <br></br>
-            since the 1500s, when an unknown printer took a galley of type and
-            scrambled it to make a type specimen book.
+          We blend unparalleled expertise with a commitment to your security.<br></br> 
+          <br></br>
+          Our team of certified professionals utilizes the latest methodologies to provide comprehensive, 
+          tailored solutions that protect your business from evolving threats. 
+          Trust us to be your partners in building a resilient and secure digital future.
+          
           </h2>
         </div>
       </Fade>
@@ -47,7 +46,7 @@ const FeatureSection = () => {
             <div className="mt-5">
               <div>
                 <h2 className="text-white opacity-50 text-xl">Year Founded</h2>
-                <h1 className="text-white text-sm">2020</h1>
+                <h1 className="text-white text-sm">2024</h1>
               </div>
               <div className="mt-5">
                 <h2 className="text-white opacity-50 text-xl">Total Funding</h2>
--- src/components/Home/header.js
+++ src/components/Home/header.js
@@ -1,7 +1,7 @@
 import React from "react"
+import Fade from "react-reveal/Fade"
 import Input from "../Atoms/input"
 import Button from "../Atoms/button"
-import Fade from "react-reveal/Fade"
 
 // Assets
 import HeaderImage from "../../images/header-main.jpg"
@@ -12,28 +12,20 @@ const Header = () => {
       <Fade>
         <div>
           <h2 className="text-white font-poppins opacity-50 font-semibold mt-20 text-xs xxs:text-xs xs:text-xs sm:text-xs md:text-sm lg:text-xl">
-            We deliver premium creative solution
+            We deliver premium cybersecurity solutions
           </h2>
 
           <h1 className="text-white font-poppins font-semibold text-2xl xxs:text-2xl xs:text-2xl sm:text-lg md:text-2xl lg:text-4xl xl:text-7xl 2xl:text-8xl">
-            Change starts with <br></br> a{" "}
+            Ensure Holistic Security <br></br> {" "}
             <span class="text-gradient bg-gradient-to-r from-pink to-purple">
-              single click.
+            every time.
             </span>
           </h1>
         </div>
       </Fade>
-      <Fade bottom>
-        <div className="mt-10 xxs:flex xxs:flex-row sm:block">
-          <Input placeholder="email."></Input>
-          <Button
-            colorClass="bg-gradient-to-r from-pink to-purple "
-            title="Request Access"
-            textColor="text-white"
-            marginClass="ml-5"
-          ></Button>
-        </div>
-      </Fade>
+
+
+
       <div className="max-w-7xl mx-auto mt-10">
         <div className="w-full h-auto overflow-hidden rounded-xl">
           <img className="w-full h-full" src={HeaderImage} alt="Header"></img>
